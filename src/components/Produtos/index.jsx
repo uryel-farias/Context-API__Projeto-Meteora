8ac76050711695adf4d89ac8a3885a85b9c474a7
Uryel Farias <uryel.farias@gmail.com>
2024-01-31 16:48:47 -0300

    Criando um hook personalizado para adicionar e remover produtos

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -2,52 +2,8 @@ import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
-import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 const Produtos = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
-
-  function adicionarProduto (novoProduto) {
-    const temOProduto = carrinho.some((itemDoCarrinho) => { itemDoCarrinho.id === novoProduto.id })
-
-    if(!temOProduto){
-      novoProduto.quantidade = 1;
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto
-      ])
-    }
-
-    setCarrinho((carrinhoAnterior) => carrinhoAnterior.map((itemDoCarrinho) => { 
-      if(itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
-      return itemDoCarrinho;
-      })
-    )
-
-    function removerProduto(id) {
-      // encontra o produto em questão
-    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-    // verifica se a quantidade é igual a um. Isso significa que este é o último 
-      // produto do tipo no carrinho
-      const ehOUltimo = produto.quantidade === 1;
-      // Com o if faz a verificação do último produto do tipo no carrinho e atualiza o estado do carrinho
-    if (ehOUltimo) {
-      return setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
-      );
-    }
-      // Se não é o último produto do carrinho, só atualizamos a quantidade, removendo um item
-    setCarrinho((carrinhoAnterior) =>
-      carrinhoAnterior.map((itemDoCarrinho) => {
-        if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade -= 1;
-        return itemDoCarrinho;
-      })
-    );
-  }
-
-
-  }
-
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
