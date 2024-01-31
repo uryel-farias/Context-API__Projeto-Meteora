ca4067ad15e0ebda1a9a091339f5e957fd485d71
Uryel Farias <uryel.farias@gmail.com>
2024-01-31 18:26:24 -0300

    Refatoração da função de adicionar e remover itens do carrinho

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -4,8 +4,20 @@ import { CarrinhoContext } from "@/context/CarrinhoContext"
 export const useCarrinhoContext = () => {
     const {carrinho, setCarrinho} = useContext(CarrinhoContext);
 
+    function mudarQuantidade(id, quantidade) {
+        carrinho.map(
+            (itemDoCarrinho) => {
+                if(itemDoCarrinho.id === id){
+                    itemDoCarrinho.quantidade += quantidade; 
+                    return itemDoCarrinho;  
+                }
+            }
+        )
+    }
+
     function adicionarProduto (novoProduto) {
-        const temOProduto = carrinho.some((itemDoCarrinho) => { itemDoCarrinho.id === novoProduto.id })
+        const temOProduto = carrinho.some(
+            (itemDoCarrinho) => (itemDoCarrinho.id === novoProduto.id ))
 
         if(!temOProduto){
         novoProduto.quantidade = 1;
@@ -15,11 +27,9 @@ export const useCarrinhoContext = () => {
         ])
         }
 
-        setCarrinho((carrinhoAnterior) => carrinhoAnterior.map((itemDoCarrinho) => { 
-        if(itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
-        return itemDoCarrinho;
-        })
-        )
+        const carrinhoAtualizado = mudarQuantidade(novoProduto.id, 1) 
+
+        setCarrinho([...carrinhoAtualizado])
     }
 
     function removerProduto(id) {
@@ -31,12 +41,9 @@ export const useCarrinhoContext = () => {
         );
         }
         
-        setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.map((itemDoCarrinho) => {
-        if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade -= 1;
-        return itemDoCarrinho;
-        })
-        );
+        const carrinhoAtualizado = mudarQuantidade(id, -1) 
+
+        setCarrinho(...carrinhoAtualizado);
         }
 
     return {
