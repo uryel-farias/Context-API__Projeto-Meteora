a34d9ec7e44ce66bef9660af69f33d88c30cb8f6
Uryel Farias <uryel.farias@gmail.com>
2024-02-01 12:06:58 -0300

    Corrigindo a função de remover produtos

--- src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
@@ -4,11 +4,8 @@ import Botao from "@/components/Botao";
 import ValorFormatado from "@/components/ValorFormatado";
 import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
-const ItemCarrinhoSuspenso = ({
-  itemCarrinho,
-  removerProdutoCarrinho,
-}) => {
-  const {adicionarProduto, removerProduto} = useCarrinhoContext();
+const ItemCarrinhoSuspenso = ({ itemCarrinho }) => {
+  const { adicionarProduto, removerProduto, removerProdutoCarrinho } = useCarrinhoContext();
   return (
     <li>
       <>
